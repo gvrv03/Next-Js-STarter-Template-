74248c740732103caafd7d5a026fd3d3880fe38d
gvrv03 <itsgaurav3112003@gmail.com>
2025-02-27 23:00:42 +0530

    Add Initial Phone Auth Template2

--- src/config/openAIConfig.js
+++
@@ -1,7 +0,0 @@
-import OpenAI from "openai";
-
-export const openai = new OpenAI({
-  apiKey:
-    "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",
-  dangerouslyAllowBrowser: true,
-});
